634932fdf5869e8d4a528ef36e2614753ebde68a
hsszyman <hsszyman@gmail.com>
2017-01-20 02:16:32 -0500

    testing with different neural library

--- net.py
+++ net.py
@@ -1,12 +1,10 @@
 #!python3
-from numpy import exp, array, random, dot
-training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
-training_set_outputs = array([[1, 1, 1, 0]]).T
-random.seed(1)
-synaptic_weights = 2 * random.random((3, 1)) - 1
-for iteration in xrange(10000):
-    output = 1 / (1 + exp(-(dot(training_set_inputs, synaptic_weights))))
-    synaptic_weights += dot(training_set_inputs.T, (training_set_outputs - output) * output * (1 - output))
-print 1 / (1 + exp(-(dot(array([1, 0, 0]), synaptic_weights))))
+import pybrain
+from pybrain.tools.shortcuts import buildNetwork
 
+aspects = {'age' : 1,'height' : 2, 'weight' : 30 }
+
+#net = buildNetwork(aspects['age'], aspects['height'], aspects['weight'])
+net = buildNetwork(2,3,1)
+print net.activate([3,1])
 
