4a25d0f9c14da0933d758f9ab153513758208083
hsszyman <hsszyman@gmail.com>
2017-01-20 13:34:08 -0500

    added dictionary for readability

--- net.py
+++ net.py
@@ -2,9 +2,9 @@
 import pybrain
 from pybrain.tools.shortcuts import buildNetwork
 
-aspects = {'age' : 1,'height' : 2, 'weight' : 30 }
+aspects = {'age' : 2, 'height' : 3, 'weight' : 1 }
 
-#net = buildNetwork(aspects['age'], aspects['height'], aspects['weight'])
-net = buildNetwork(2,3,1)
-print net.activate([3,1])
+net = buildNetwork(aspects['age'], aspects['height'], aspects['weight'])
+#net = buildNetwork(2,3,1)
+print net.activate([2,3]) # This gives random answers every time the program is run. Why?
 
